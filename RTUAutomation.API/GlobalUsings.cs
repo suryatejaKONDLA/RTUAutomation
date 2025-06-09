75f42bac58a5d5ec18e98084d8a7a495864c5691
Suryateja KONDLA <kondla.suryateja1206@gmail.com>
2025-06-09 21:52:19 +0530

    Add project files.

---
+++ RTUAutomation.API/GlobalUsings.cs
@@ -0,0 +1,4 @@
+// Global using directives
+
+global using Serilog;
+global using Serilog.Events;
---
+++ RTUAutomation.API/Program.cs
@@ -0,0 +1,59 @@
+const string appName = "RTUAutomation.API";
+const string apiTitle = "RTU Automation API";
+const string apiVersion = "v1";
+const string openApiPath = $"/{apiVersion}/api.json";
+const string swaggerUiPath = "/swagger";
+const string generalLogFile = "Logs/Log-.txt";
+const string authLogFile = "Logs/Authentication-Log-.txt";
+
+Directory.CreateDirectory("Logs");
+
+const string logTemplate = "[{Timestamp:yyyy-MM-dd HH:mm:ss}] [{Level:u3}] {Message}{NewLine}{Exception}";
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+    .MinimumLevel.Override("System", LogEventLevel.Warning)
+    .Enrich.FromLogContext()
+    .Enrich.WithProperty("Application", appName)
+    .WriteTo.Console(outputTemplate: logTemplate)
+    .WriteTo.File(generalLogFile, LogEventLevel.Information, rollingInterval: RollingInterval.Day,
+        retainedFileCountLimit: 30, outputTemplate: logTemplate)
+    .WriteTo.Logger(lc => lc
+        .Filter.ByIncludingOnly(le =>
+            le.Properties.ContainsKey("SourceContext") &&
+            le.Properties["SourceContext"].ToString().Contains("RTUAutomation.Services.AuthenticationServices"))
+        .WriteTo.File(authLogFile, LogEventLevel.Information, rollingInterval: RollingInterval.Day,
+            retainedFileCountLimit: 30, outputTemplate: logTemplate))
+    .CreateLogger();
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseSerilog();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApiDocument(config =>
+{
+    config.Title = apiTitle;
+    config.Version = apiVersion;
+});
+
+builder.Services.AddHealthChecks();
+
+var app = builder.Build();
+
+app.UseSerilogRequestLogging();
+app.UseHttpsRedirection();
+
+app.UseOpenApi(opt => opt.Path = openApiPath);
+app.UseSwaggerUi(settings =>
+{
+    settings.Path = swaggerUiPath;
+    settings.DocumentTitle = apiTitle;
+    settings.PersistAuthorization = true;
+    settings.DocExpansion = "none";
+    settings.SwaggerRoutes.Clear();
+    settings.SwaggerRoutes.Add(new("RTU API", openApiPath));
+});
+
+app.MapOpenApi();
+await app.RunAsync();
